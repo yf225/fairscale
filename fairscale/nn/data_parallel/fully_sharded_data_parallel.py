0ecda856207e8ba34c3e42c46fa7a3815e8aa84f
Anjali Sridhar <anj@devfair0443.h2.fair>
2021-09-27 19:00:41 -0700

    update comment

--- fairscale/nn/data_parallel/fully_sharded_data_parallel.py
+++ fairscale/nn/data_parallel/fully_sharded_data_parallel.py
@@ -708,6 +708,9 @@ class FullyShardedDataParallel(nn.Module):
         part of the model. This call cannot be made when ssd_offload is set because
         parameter data will not be loaded.
         """
+        # TODO(anj): Use `copy_into_tensor` in order to provide a copy of the 
+        # parameters and not the actual parameters. Ideally we don't users to operate on 
+        # actual params.
         if self.ssd_offload and self.ssd_buffer.buffer is None:
             self.ssd_buffer.from_disk(self.buffer_size)
 
@@ -736,6 +739,9 @@ class FullyShardedDataParallel(nn.Module):
         If you want the full param to be returned, you should call this function
         under a `summon_full_params` context when using flattened or original params.
         """
+        # TODO(anj): Use `copy_into_tensor` in order to provide a copy of the 
+        # parameters and not the actual parameters. Ideally we don't users to operate on 
+        # actual params.
         if self.ssd_offload and self.ssd_buffer.buffer is None:
             self.ssd_buffer.from_disk(self.buffer_size)
 
