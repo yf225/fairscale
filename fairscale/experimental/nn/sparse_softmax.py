added checkpoint, much faster and smaller memory, tested grad computation is correct

fairscale/experimental/nn/sparse_softmax.py
@@ -4,11 +4,12 @@
 # LICENSE file in the root directory of this source tree.
 
 
-from typing import Any, Tuple
+from typing import Any, List, Tuple
 
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.utils.checkpoint import checkpoint
 
 from .triton import fused_forward  # type: ignore
 
@@ -45,7 +46,7 @@ def get_data(
     """ Utility function for getting some tensors for testing and benchmarking."""
     (tokens, d1), (d2, vocabs) = shape
     assert d1 == d2
-    input = torch.rand(tokens, d1, device=device, dtype=dtype)
+    input = torch.rand(tokens, d1, device=device, dtype=dtype).requires_grad_(True)
     weight = nn.Linear(d2, vocabs, bias=False, device=device, dtype=dtype).weight
     target = (torch.rand(tokens, device=device) * vocabs).long()
     return input, weight, target
@@ -146,21 +147,12 @@ class TorchFuseAllTiled(nn.Module):
         self.reduction = "sum"
         assert self.reduction in ["sum", "mean", "none"]
 
-    def forward(self, *input: Any, **kwargs: Any) -> Any:
-        assert kwargs == {}
-        input, target = input
-        assert isinstance(input, torch.Tensor)
-        assert isinstance(target, torch.Tensor)
-        if len(input.shape) == 3:
-            input = input.reshape(-1, input.shape[2])
-        if len(target.shape) == 2:
-            target = target.reshape(-1)
+    def get_maxs(self, input: torch.Tensor) -> List[torch.Tensor]:
         tokens, d_model = input.shape
         vocab, d2 = self.proj_weight.shape
         assert d_model == d2
         inputs = torch.split(input, next_power_of_2_or_max(tokens // self.tf_in, tokens), 0)
         weights = torch.split(self.proj_weight, next_power_of_2_or_max(vocab // self.tf_w, vocab), 0)
-        # Get maxs
         maxs = []
         for i in inputs:
             m = None  # max with (tokens_tile,) shape
@@ -173,11 +165,16 @@ class TorchFuseAllTiled(nn.Module):
                     m = _m
                 else:
                     m = torch.max(m, _m)
+            assert m is not None
             maxs.append(m)  # (tokens_tile,)
-        maxs_tensor = torch.cat(maxs)  # (tokens,)
-        assert maxs_tensor.shape == (tokens,)
+        return maxs
 
-        # Get sums.
+    def get_sums(self, input: torch.Tensor, maxs: List[torch.Tensor]) -> List[torch.Tensor]:
+        tokens, d_model = input.shape
+        vocab, d2 = self.proj_weight.shape
+        assert d_model == d2
+        inputs = torch.split(input, next_power_of_2_or_max(tokens // self.tf_in, tokens), 0)
+        weights = torch.split(self.proj_weight, next_power_of_2_or_max(vocab // self.tf_w, vocab), 0)
         sums = []
         for idx, i in enumerate(inputs):
             s = None  # sum with (tokens_tile,) shape
@@ -190,7 +187,29 @@ class TorchFuseAllTiled(nn.Module):
                     s = _s
                 else:
                     s += _s
+            assert s is not None
             sums.append(s)  # (tokens_tile,)
+        return sums
+
+    def forward(self, *input: Any, **kwargs: Any) -> Any:
+        assert kwargs == {}
+        input, target = input
+        assert isinstance(input, torch.Tensor)
+        assert isinstance(target, torch.Tensor)
+        assert input.requires_grad
+        if len(input.shape) == 3:
+            input = input.reshape(-1, input.shape[2])
+        if len(target.shape) == 2:
+            target = target.reshape(-1)
+        tokens, d_model = input.shape
+
+        # Get maxs
+        maxs = checkpoint(self.get_maxs, input)
+        maxs_tensor = torch.cat(maxs)  # (tokens,)
+        assert maxs_tensor.shape == (tokens,)
+
+        # Get sums.
+        sums = checkpoint(self.get_sums, input, maxs)
         sums = torch.cat(sums)  # (tokens,)
         assert sums.shape == (tokens,)
 